a646ec7d735c6b9a0f66f645c213c9292bad552f
frogk <greenfrog0099@gmail.com>
2022-07-28 14:22:04 +0900

    Eduguide 공지사항 부분 완성

---
+++ src/components/Main/OnAndUp/MainItem/EduGuide/EduNoticeDetail.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const EduNoticeDetail = ({lists,setContent}) => {
+  return (
+    <div className='edu_detail_wrap'>
+      <div className="header">
+        <div className="title_area">
+          <span className='category'>{lists.category}</span>
+          <span className='title'>{lists.title}</span>
+        </div>
+        <p className='date'>{lists.date}</p>
+      </div>
+      <div className="main">
+        <div className="img_box">
+          <img src={lists.content_img} alt={lists.id} />
+        </div>
+        <div className="text">
+          <span>{lists.content_text}</span>
+        </div>
+      </div>
+      <div className="footer">
+        <button className="btn" onClick={() => setContent(true)}>
+          목록보기
+        </button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default EduNoticeDetail;
