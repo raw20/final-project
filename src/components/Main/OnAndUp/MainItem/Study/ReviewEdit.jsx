e87ff649beefe6c2e26c48ffbff300e467bd7446
OTecHeun <tjdgms1145a@naver.com>
2022-07-31 17:51:22 +0900

    이미지 넣고 사이즈 등

--- src/components/Main/OnAndUp/MainItem/Study/ReviewEdit.jsx
+++ src/components/Main/OnAndUp/MainItem/Study/ReviewEdit.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useParams, Link } from 'react-router-dom';
-
 import Editor from './_commos/Editor'
-
 import { setItem , getItem } from './lib/storage';
 
 function ReviewEdit({datas, addData, setData, saveReviewData}) {
@@ -26,11 +24,16 @@ function ReviewEdit({datas, addData, setData, saveReviewData}) {
 
 
     return (
-        <div>
+        <div className='.detail_practice'>
             <Editor id={id} isNew={isNew} data={ isNew ? '' : datas[moduleId]} addData={addData} setData={setData} />
-            <Link to = {`../aboutCompany/${id}`} className="submitBtnArea">
-                <button className='submitBtn' onClick={saveReviewData}>제출하기</button>
-            </Link>
+            <div className='.detail_practice_btnArea'>
+                <Link to = {`../aboutCompany/${id}`} className="submitBtnArea_save">
+                    <button className='tempSaveBtn' onClick={saveReviewData}>저장하기</button>
+                </Link>
+                <Link to = {`../aboutCompany/${id}`} className="submitBtnArea">
+                    <button className='submitBtn' onClick={saveReviewData}>제출하기</button>
+                </Link>
+            </div>
         </div>
         
     );
