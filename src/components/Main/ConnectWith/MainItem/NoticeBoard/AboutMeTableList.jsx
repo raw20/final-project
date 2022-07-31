ccb9e95ff045f45d90d5d4b21182b1d0d5326278
raw20 <raw1250@gmail.com>
2022-07-31 15:34:21 +0900

    조회수 rollback

--- src/components/Main/ConnectWith/MainItem/NoticeBoard/AboutMeTableList.jsx
+++ src/components/Main/ConnectWith/MainItem/NoticeBoard/AboutMeTableList.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { countView } from "../../../../../app/detailSlice";
 import AboutMeDetail from "./AboutMeDetail";
 import TableSearchBarTwo from "./TableSearchBarTwo";
 const AboutMeTableList = ({ posts, totalPosts, content, setContent }) => {
   const [index, setIndex] = useState();
+  const dispatch = useDispatch();
   const likeValue = useSelector((state) => state.item.data);
   function onClick(index) {
     setContent(false);
     setIndex(index);
+    dispatch(
+      countView({
+        id: posts[index].id,
+        title: posts[index].title,
+        writer: posts[index].writer,
+        content_text: posts[index].content_text,
+        content_img:
+          "http://conference.exc.co.kr/chemiverse/assets/img/img-board-view.png",
+        file: "N",
+        like: posts[index].like,
+        views: posts[index].views,
+        date: posts[index].date,
+      })
+    );
   }
   return (
     <>
