b6d9e11c11d94e03a6b14ae042687c353a04aece
frogk <greenfrog0099@gmail.com>
2022-07-31 18:21:47 +0900

    게시판 디테일 페이지 반응형 수정

--- src/components/Main/ConnectWith/MainItem/NoticeBoard/NoticeDetail.jsx
+++ src/components/Main/ConnectWith/MainItem/NoticeBoard/NoticeDetail.jsx
@@ -5,20 +5,20 @@ function NoticeDetail({ posts, setContent }) {
   return (
     <div className="notice-detail-wrap">
       <div className="notice-detail-header">
-        <div className="notice-detail-category">{posts.category}</div>
-        <h1 className="notice-detail-title"> {posts.title}</h1>
-      </div>
-
-      <div className="notice-detail-main">
+        <div className="notice-detail-title-area">
+          <div className="notice-detail-category">{posts.category}</div>
+          <h1 className="notice-detail-title"> {posts.title}</h1>
+        </div>
         <p className="notice-detail-date">작성일 : {posts.date}</p>
+      </div>
+      <div className="notice-detail-main">
         <div className="notice-detail-img">
           <img src={posts.content_img} alt={posts.id} />
           <div className="notice-detail-text">{posts.content_text}</div>
         </div>
       </div>
       <div className="notice-detail-footer">
-        <div className="notice-detail-file">첨부파일 {posts.file}</div>
-        <button className="notice-btn" onClick={() => setContent(true)}>
+        <button className="notice-detail-btn" onClick={() => setContent(true)}>
           목록보기
         </button>
       </div>
